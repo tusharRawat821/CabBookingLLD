c7c255f4b2a27ad7dcb10a3df73c92fc89b68a79
Tushar Rawat <tusharrawat821@gmail.com>
2021-11-13 17:19:53 +0530

    Update the main class.

--- src/main/java/com/lld/cabbooking/CabBookingApp.java
+++ src/main/java/com/lld/cabbooking/CabBookingApp.java
@@ -3,30 +3,7 @@ package com.lld.cabbooking;
 public class CabBookingApp {
 	
 	public static void main(String[] args) {
-//		CabsManager cabsManager = new CabsManager();
-//		PricingStrategy pricingStrategy = new PricingStrategy() {
-//			
-//			@Override
-//			public Double calculateFare(Location origin, Location dest) {
-//				// TODO Auto-generated method stub
-//				return null;
-//			}
-//		};
-//		CabMatchingStrategy cabMatchingStrategy = new CabMatchingStrategy() {
-//			
-//			@Override
-//			public Optional<Cab> matchCabToRider(Rider rider, List<Cab> closeByCabs, Location origin, Location dest) {
-//				// TODO Auto-generated method stub
-//				return null;
-//			}
-//		};
-//		TripsManager tripsManager = new TripsManager(cabsManager, pricingStrategy, cabMatchingStrategy);
-//		CabsController cabsController = new CabsController(cabsManager, tripsManager);
-//		
-//		cabsController.registerCab("123", "Raman");
-//		cabsController.updateCabAvailability("123", false);
-//		cabsController.updateCabLocation("123", 4.5, 102.6);
-		
+		// we can also simulate the behaviour here.
 	}
 	
 }
